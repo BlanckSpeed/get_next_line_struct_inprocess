33d8cdef6d54ff0611d1128e3e8ed867296b2b58
BlanckSpeed <inrodrigoant@gmail.com>
2024-07-25 03:14:51 +0200

    feat: add get_next_line header for making structure of dynamic list for BUFFER

--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,22 @@
 /*   By: rlendine <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 12:28:21 by rlendine          #+#    #+#             */
-/*   Updated: 2024/07/11 12:29:16 by rlendine         ###   ########.fr       */
+/*   Updated: 2024/07/25 02:06:35 by rodrigo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-char	*get_next_line(int fd);
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFFER_SIZE 1024
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <stdlib.h>
 
+typedef struct s_list
+{
+	char			*line_read;
+	struct s_list	*next_line;
+}	t_list;
+
+char	*get_next_line(int fd);
+#endif
